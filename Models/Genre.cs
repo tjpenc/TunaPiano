b8886053a2a98d9729d77297dc45380e88fc9343
Thomas Pencarinha <tjpencarinha@gmail.com>
2023-09-02 14:37:01 -0400

    Created classes for the project

---
+++ Models/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TunaPiano.Models
+{
+    public class Genre
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
