0eef0acf3be2855598714d14303526cc7d5f39c6
Thomas Pencarinha <tjpencarinha@gmail.com>
2023-09-02 15:36:10 -0400

    setup DbContext file and created database

--- Models/SongGenre.cs
+++ Models/SongGenre.cs
@@ -6,7 +6,9 @@ namespace TunaPiano.Models
     {
         [Required]
         public int SongId { get; set; }
+        public Song Song { get; set; }
         [Required]
         public int GenreId { get; set; }
+        public Genre Genre { get; set; }
     }
 }
